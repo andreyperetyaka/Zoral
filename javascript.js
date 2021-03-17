101eac3f589aeb1f2bc5f1ff2e26d2d21abadf40
Andrey Peretyaka <andreyperetyaka@gmail.com>
2021-03-17 16:11:30 +0200

    js part

---
+++ javascript.js
@@ -0,0 +1,59 @@
+// 1.1
+
+Date.prototype.daysTo = function (date) {
+  if (!(date instanceof Date))
+    throw new TypeError('Argument should be an instance of Date!');
+  const msPerDay = 24 * 60 * 60 * 1000;
+  return Math.floor(Math.abs((this - date) / msPerDay));
+};
+
+// 1.2
+
+const formatOrders = function (orders) {
+  if (!(orders instanceof Array))
+    throw new TypeError('Orders should be an instance of Array!');
+  return orders
+    .map((order) => {
+      if (!(order instanceof Object))
+        throw new TypeError('Orders should be an Array of Objects!');
+      let { amount, quantity } = order;
+      if (!(typeof amount === 'number') || amount < 0)
+        throw new Error(
+          `Order: ${JSON.stringify(
+            order
+          )} should have value of amount which is non-negative number`
+        );
+      if (!(typeof quantity === 'number') || quantity < 0)
+        throw new Error(
+          `Order: ${JSON.stringify(
+            order
+          )} should have value of quantity which is non-negative number`
+        );
+      let Total = amount * quantity;
+      return { ...order, Total };
+    })
+    .sort((a, b) => a.Total - b.Total);
+};
+
+// 1.3
+
+const getProjection = function (src, proto) {
+  if (!(src instanceof Object) || !(proto instanceof Object))
+    throw new Error(
+      'Please provide two arguments with Object type to the function'
+    );
+  return Object.assign(
+    {},
+    ...Object.keys(proto).map((key) => {
+      if (
+        proto[key] &&
+        proto[key] instanceof Object &&
+        src[key] instanceof Object
+      ) {
+        return { [key]: getProjection(src[key], proto[key]) };
+      } else {
+        return { [key]: src[key] };
+      }
+    })
+  );
+};
